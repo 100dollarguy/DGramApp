6dcc3c5125ac29961b70dae373feb9017bdccb12
ROHAN D R <rohandrgpg@gmail.com>
2025-06-14 16:12:54 +0530

    Testing with other accounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     ganache: {
       url: "http://127.0.0.1:8545", // default Ganache RPC
       accounts: [
-        "0x862ab19f4e41a1dd51b76c380b0fbe76a1d926fbc5d25d0bf0ba425aeb165ad3"
+        "0x8c237f82b867e049b892ce0cee503609c7ee3a3514b7fe57a8b5dd1f0d5f5c81"
       ]
     }
   }
